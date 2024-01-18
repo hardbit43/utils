65d3bd681745c0704fa6621fdc1f40ce29a197cb
Александр Зайцев <azaytsev@ohta.local>
2024-01-18 18:30:21 +0300

    v4

--- utils.go
+++ utils.go
@@ -17,3 +17,12 @@ func InSliseInt(a []int, x int) bool {
 	}
 	return false
 }
+
+func InSliceFloat(a []float64, x float64) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
